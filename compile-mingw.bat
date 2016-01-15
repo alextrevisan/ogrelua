520f58752b7c8e3b898efbd54069c893b1cf186f
Alexandro Trevisan <alexbaixista@gmail.com>
2016-01-15 16:55:17 -0300

    Removed .bat file

--- compile-mingw.bat
+++
@@ -1,7 +0,0 @@
-g++.exe -std=c++11 -Wall -DBUILD_DLL -O3 -I..\LuaJIT-2.0.4\src -I..\OgreSDK\include\OGRE -c FrameListener.cpp -o FrameListener.o
-
-g++.exe -std=c++11 -Wall -DBUILD_DLL -O3 -I..\LuaJIT-2.0.4\src -I..\OgreSDK\include\OGRE -c main.cpp -o main.o
-
-g++.exe -shared  -Wl,--dll  FrameListener.o main.o  -o ogrelua.dll -s ..\LuaJIT-2.0.4\src\libluajit.a ..\OgreSDK\lib\release\libOgreTerrain.dll.a ..\OgreSDK\lib\release\libOgreMain.dll.a
-
-pause
